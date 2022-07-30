16d3988e19381bfdc190abe07d2fe0ee3f8da39a
mwynyard <101786771+mwynyard@users.noreply.github.com>
2022-07-31 07:56:02 +1000

    first commit

---
+++ Response.js
@@ -0,0 +1,654 @@
+const wfO = {
+  payload: {
+    nodes: [
+      {
+        _id: "ea5d802d-c3c2-4b20-a4d4-fcaa7685db39",
+        tag: "div",
+        classes: [],
+        children: [
+          "d762a818-4680-4b11-bdb7-9859c2659bc7",
+          "755f7b6c-da6b-4d97-ba84-764567d596d7",
+        ],
+        data: {
+          tag: "div",
+          attr: { id: "wrapper-id", className: "wrapper wrapper-first" },
+        },
+      },
+      {
+        _id: "bb60589d-0a2e-4af6-b1b8-892e664c36ef",
+        tag: "div",
+        classes: [],
+        children: ["f722b2ee-cc4f-43e2-8f8d-a2a3c46e5dcd"],
+        data: {
+          tag: "div",
+          attr: { id: "top-section", className: "section" },
+        },
+      },
+      {
+        _id: "907647bf-6f50-4426-820c-bc35e1bee448",
+        tag: "div",
+        classes: [],
+        children: ["b5425717-551d-4611-8028-bfa2e6d5007f"],
+        data: { tag: "div", attr: { className: "container" } },
+      },
+      {
+        _id: "aaf71495-fca6-471e-b051-daef88623b50",
+        tag: "div",
+        classes: [],
+        children: [
+          "cc17bea7-72e0-44ea-b225-66e3afb46f2a",
+          "a004ab08-9ca8-46d3-b63d-ddfb84eba632",
+          "71925b0a-3952-457c-944e-ffed47e4454a",
+        ],
+        data: { tag: "div", attr: { className: "intro-wrap" } },
+      },
+      {
+        _id: "51542557-8e60-4847-90a5-8059b3663bb5",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "name-text" } },
+      },
+      {
+        _id: "06499e08-75f1-4cca-b345-444ce6b74cad",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "paragraph-light" } },
+      },
+      {
+        _id: "58b36f29-2a22-4726-90c0-560798204b81",
+        tag: "h1",
+        classes: [],
+        children: [],
+        data: {
+          tag: "h1",
+          attr: { id: "h1-id", className: "heading-jumbo" },
+        },
+      },
+      {
+        _id: "5f7de2fa-cb75-4374-b120-0e80ea295253",
+        tag: "div",
+        classes: [],
+        children: [
+          "bb312c62-b472-4c10-9b54-78a8e972abfd",
+          "727c80e2-ac96-4405-8818-23bdc1dd2211",
+        ],
+        data: {
+          tag: "div",
+          attr: { id: "bottom-section", className: "section" },
+        },
+      },
+      {
+        _id: "a67ce8a1-051c-4fc0-88d2-d12ebc9e0d8c",
+        tag: "div",
+        classes: [],
+        children: [
+          "ddb0c25f-8964-4d6d-8e23-ed9538af3ed2",
+          "26b1a754-1107-42f5-89c7-411376f95710",
+          "8ff815fe-7c4b-402b-b2b7-91c42a37ddbb",
+          "9ddc2726-ac0e-4053-9c06-9d6ac8527e25",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "works-grid",
+            className: "w-layout-grid works-grid",
+          },
+        },
+      },
+      {
+        _id: "b7d130a2-cbdf-40c2-95b9-35743dbc18b5",
+        tag: "div",
+        classes: [],
+        children: [
+          "5032c212-f22e-47b3-a82b-37fe8538fead",
+          "339f164e-e6fe-43f2-aab9-8e3f4a15dddc",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fa5-571089c5",
+          },
+        },
+      },
+      {
+        _id: "7491d410-1f07-4231-ae15-f2fea77fd4be",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-1",
+            className: "work-image cc-work-1 w-inline-block",
+          },
+        },
+      },
+      {
+        _id: "1681d7f6-4dab-4f87-9208-244d5a4429f8",
+        tag: "div",
+        classes: [],
+        children: [
+          "3b9d1c3d-ca5e-4fa8-bc09-1df78dc4c9f8",
+          "a1f74464-02b6-4344-92c5-e253fe8e3d95",
+        ],
+        data: { tag: "div", attr: { className: "work-description" } },
+      },
+      {
+        _id: "3d97a595-f0ba-49b7-89cd-db1ef3719992",
+        tag: "a",
+        classes: [],
+        children: ["fc643ed9-5a55-4647-a7b1-2ad78a19399e"],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-1",
+            className: "project-name-link",
+          },
+        },
+      },
+      {
+        _id: "62a9ecaf-3566-402b-9fe5-258eff46fcb3",
+        tag: "br",
+        classes: [],
+        children: [],
+        data: { tag: "br", attr: {} },
+      },
+      {
+        _id: "69036ad9-c826-4825-ba06-45e02ea9a7b7",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "paragraph-light" } },
+      },
+      {
+        _id: "35795976-d872-45b8-aa0c-4155afc7cca3",
+        tag: "div",
+        classes: [],
+        children: [
+          "2bda3325-04f0-42e1-ad3e-6d20290c148c",
+          "c28eaa1f-818e-479a-830d-e9f5dc7da388",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fad-571089c5",
+          },
+        },
+      },
+      {
+        _id: "5e93bde6-4633-42d3-bc7a-ae58b863ba9b",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-2",
+            className: "work-image cc-work-2 w-inline-block",
+          },
+        },
+      },
+      {
+        _id: "009ebca5-a451-4ba7-939a-5896df83870e",
+        tag: "div",
+        classes: [],
+        children: [
+          "9a12544b-2fb5-47a4-a104-c5e5cc91f942",
+          "44baa283-6ed4-417f-908d-df757a3e6fcc",
+        ],
+        data: { tag: "div", attr: { className: "work-description" } },
+      },
+      {
+        _id: "0f7e55f8-0486-4cc3-b2e5-9ac5af7c5c52",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-2",
+            className: "project-name-link",
+          },
+        },
+      },
+      {
+        _id: "59972f3a-f2cc-4700-b43d-368fcd62d5d7",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "paragraph-light" } },
+      },
+      {
+        _id: "dd271d38-dcf4-43a3-9679-f57d548c9c23",
+        tag: "div",
+        classes: [],
+        children: [
+          "122e4004-ca27-40fe-beca-947ee24b4a4d",
+          "371e29ca-7a8b-46e5-ac49-258d1b494d37",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fb4-571089c5",
+          },
+        },
+      },
+      {
+        _id: "50b51442-7e33-4359-95e8-cfef0511c0d4",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-3",
+            className: "work-image cc-work-3 w-inline-block",
+          },
+        },
+      },
+      {
+        _id: "e977300a-db27-4208-8731-b56523330e2f",
+        tag: "div",
+        classes: [],
+        children: [
+          "3095c4df-47ed-4767-8126-d17935efffb9",
+          "251866e1-ca0b-4acb-b107-64e4aa60a8b1",
+        ],
+        data: { tag: "div", attr: { className: "work-description" } },
+      },
+      {
+        _id: "b8c3f413-ddac-40bf-8c00-ab9d2b4643fd",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-3",
+            className: "project-name-link",
+          },
+        },
+      },
+      {
+        _id: "054062ab-e348-4777-a787-e82c092a3d48",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "paragraph-light" } },
+      },
+      {
+        _id: "e1e9c0d7-42c1-4e66-a7d2-12c7c16ddc9a",
+        tag: "div",
+        classes: [],
+        children: [
+          "4176bc0c-1dcc-44be-b4fa-82325ab65536",
+          "5dcbf635-0bcd-4e17-a6f4-0443161a106c",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fbb-571089c5",
+          },
+        },
+      },
+      {
+        _id: "95b13447-3db1-427a-a5a4-db3c15645d5b",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-4",
+            className: "work-image cc-work-4 w-inline-block",
+          },
+        },
+      },
+      {
+        _id: "99570b83-8bde-4c2b-8633-cd4cff5090b9",
+        tag: "div",
+        classes: [],
+        children: [
+          "5938e0ef-09cf-4bbc-b966-4784c7806df4",
+          "54c87367-52e6-497a-8a16-c5f150bd6324",
+        ],
+        data: { tag: "div", attr: { className: "work-description" } },
+      },
+      {
+        _id: "5fa83396-5909-4fe4-ae2d-1cb4b45b95bd",
+        tag: "a",
+        classes: [],
+        children: [],
+        data: {
+          tag: "a",
+          attr: {
+            href: "/work/project-4",
+            className: "project-name-link",
+          },
+        },
+      },
+      {
+        _id: "2774e105-a7a3-4c6e-a727-90bcaf2014d3",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "paragraph-light" } },
+      },
+      {
+        _id: "ed4d285a-a9ba-4cf1-86b5-198d37e26717",
+        tag: "div",
+        classes: [],
+        children: [
+          "ab81b4b6-0a5a-4c93-bd55-6ae8072c1178",
+          "c78c7f4b-7c54-4a46-8b2b-e5ec25e11fa4",
+        ],
+        data: { tag: "div", attr: { className: "container" } },
+      },
+      {
+        _id: "e41051ea-030d-4793-bc3c-75323573a43e",
+        tag: "div",
+        classes: [],
+        children: [
+          "a4262f8b-a14f-49f8-bca1-334341bb6f23",
+          "59357d3a-bdc0-4cf1-8d4d-26f99203c866",
+        ],
+        data: {
+          tag: "div",
+          attr: { className: "carrer-headline-wrap" },
+        },
+      },
+      {
+        _id: "515d3c35-9f4c-4acf-9452-50f8dcad165b",
+        tag: "h2",
+        classes: [],
+        children: [],
+        data: { tag: "h2", attr: {} },
+      },
+      {
+        _id: "b3d743d6-bc28-424a-8f25-ccd516d74a54",
+        tag: "p",
+        classes: [],
+        children: [],
+        data: { tag: "p", attr: { className: "paragraph-light" } },
+      },
+      {
+        _id: "9a908ff2-9490-42ee-9fdd-2f6d7e57c43e",
+        tag: "div",
+        classes: [],
+        children: [
+          "a5a9328b-248a-4f07-b9d9-0b627e021880",
+          "5bc601df-e4d3-4113-8a75-766020deadea",
+          "f1b92859-45c5-40a5-91bd-7ce0280d7ce9",
+          "3a177e41-add2-445f-ac10-f0d9fef96c5d",
+        ],
+        data: {
+          tag: "div",
+          attr: { className: "w-layout-grid work-experience-grid" },
+        },
+      },
+      {
+        _id: "05600404-13c9-4759-95c1-d211c5cc14d0",
+        tag: "div",
+        classes: [],
+        children: [
+          "4eb5cd8f-e281-445d-9950-a825b5f704e8",
+          "860ba3a9-bbf9-4e47-95dd-f6e37715d935",
+          "af9ef969-5733-40c3-8519-63516fb903b1",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fc9-571089c5",
+            className: "work-position-wrap",
+          },
+        },
+      },
+      {
+        _id: "2f4bec72-cff8-4300-93b0-c7c9501a05a7",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "position-name-text" } },
+      },
+      {
+        _id: "9070919d-eff9-49ef-ad0f-95c485985284",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-light cc-position-name" },
+        },
+      },
+      {
+        _id: "377d48b7-1f01-4a06-ac7d-8e85d1bf8a68",
+        tag: "div",
+        classes: [],
+        children: ["319ac2bd-38c6-4ab7-8d75-370a59399413"],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-tiny cc-paragraph-tiny-light" },
+        },
+      },
+      {
+        _id: "951fa036-2493-455e-a03a-03fafae764b7",
+        tag: "br",
+        classes: [],
+        children: [],
+        data: { tag: "br", attr: {} },
+      },
+      {
+        _id: "bf1b187c-1860-4e01-91ea-2f9f1b6e196c",
+        tag: "div",
+        classes: [],
+        children: [
+          "e1e18424-4fa9-4913-9016-0b922c1aa398",
+          "89eccd6f-147e-4f05-861d-d0148e78f83c",
+          "8bd0ff51-f443-4cbe-95e8-0a7ac26170c4",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fd1-571089c5",
+            className: "work-position-wrap",
+          },
+        },
+      },
+      {
+        _id: "50800716-2ead-4f17-8aee-dc0eaf256db6",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "position-name-text" } },
+      },
+      {
+        _id: "df0663a6-a015-4b73-8ad5-808215313540",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-light cc-position-name" },
+        },
+      },
+      {
+        _id: "729b2b31-b758-452a-8846-3a2680de63d8",
+        tag: "div",
+        classes: [],
+        children: ["c47d6c63-ab16-4b1d-b6f7-3dffc97e31b4"],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-tiny cc-paragraph-tiny-light" },
+        },
+      },
+      {
+        _id: "9668cec7-5935-451e-bd18-d866c3cf43fa",
+        tag: "br",
+        classes: [],
+        children: [],
+        data: { tag: "br", attr: {} },
+      },
+      {
+        _id: "b4ca5e66-c94d-4f19-b065-af5f92143c75",
+        tag: "div",
+        classes: [],
+        children: [
+          "c06bfb52-e32e-4e30-a572-a7f70c358d30",
+          "64158b13-18a7-43bb-b8f0-feaae9abbaeb",
+          "bc461e28-3137-4cad-adbb-790458c19547",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fd9-571089c5",
+            className: "work-position-wrap",
+          },
+        },
+      },
+      {
+        _id: "519c465d-9e42-4471-8a81-0ae1655daa90",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "position-name-text" } },
+      },
+      {
+        _id: "50969c0d-677a-4910-b327-44faaf3576bd",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-light cc-position-name" },
+        },
+      },
+      {
+        _id: "ee64fd2f-b39e-4ffd-ad97-e7203453c7e5",
+        tag: "div",
+        classes: [],
+        children: ["1346e3cc-24ab-4830-b6e8-79bd171ee605"],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-tiny cc-paragraph-tiny-light" },
+        },
+      },
+      {
+        _id: "d5647d59-d129-4216-8259-748a71def08d",
+        tag: "br",
+        classes: [],
+        children: [],
+        data: { tag: "br", attr: {} },
+      },
+      {
+        _id: "505e0d70-514c-4748-9731-96da4cc88d14",
+        tag: "div",
+        classes: [],
+        children: [
+          "4974a6b4-b9b6-4a01-8981-98ca986b5d94",
+          "198dff8d-bdb8-4d7a-8cef-390a37ba0132",
+          "f00f682f-5f91-45ae-874d-3824efaabf13",
+        ],
+        data: {
+          tag: "div",
+          attr: {
+            id: "w-node-dff26f68-5731-3efa-d3dd-8f65c72d8fe1-571089c5",
+            className: "work-position-wrap",
+          },
+        },
+      },
+      {
+        _id: "1acc9095-2bc7-4707-8b01-244d33b27c26",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: { tag: "div", attr: { className: "position-name-text" } },
+      },
+      {
+        _id: "efb5d136-240b-40df-a1ef-07a317671288",
+        tag: "div",
+        classes: [],
+        children: [],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-light cc-position-name" },
+        },
+      },
+      {
+        _id: "b9997e40-7da8-4a39-9bf2-3c1539e33dc5",
+        tag: "div",
+        classes: [],
+        children: ["2d2d5f82-c0c9-4abc-bb76-6e54456fef68"],
+        data: {
+          tag: "div",
+          attr: { className: "paragraph-tiny cc-paragraph-tiny-light" },
+        },
+      },
+      {
+        _id: "cb7cd374-1de1-42ac-96db-f1d31bcd43bf",
+        tag: "br",
+        classes: [],
+        children: [],
+        data: { tag: "br", attr: {} },
+      },
+    ],
+    styles: [
+      {
+        fake: false,
+        _id: "04e15b52-201a-46da-8242-ff3cbca9797c",
+        type: "class",
+        name: "root",
+        styleLess: '""',
+        children: [
+          { _id: "6ad95a70-54d9-461c-a36b-f239e5e86290" },
+          { _id: "8fcb01dd-c814-42e4-97ff-0bc220e12b6b" },
+          { _id: "31a1eb9e-c934-48f8-8412-42cc2998ea72" },
+        ],
+        comb: "",
+        namespace: "",
+        selector: null,
+        variants: { small: { styleLess: [] } },
+      },
+      {
+        fake: false,
+        _id: "bf8600c3-5298-4c40-9dff-b4757d5cc54d",
+        type: "id",
+        name: "demo",
+        styleLess: '"flex:1 100%;background:red"',
+        children: [],
+        comb: "",
+        namespace: "",
+        selector: null,
+        variants: {
+          small: { styleLess: ["flex:1 100%", "background:red"] },
+        },
+      },
+      {
+        fake: false,
+        _id: "562f4f92-423d-497f-a36b-e6d7c0d9a073",
+        type: "class",
+        name: "breadcrumb-wrapper",
+        styleLess: '"flex:1 100%"',
+        children: [],
+        comb: "",
+        namespace: "",
+        selector: null,
+        variants: { small: { styleLess: ["flex:1 100%"] } },
+      },
+      {
+        fake: false,
+        _id: "94c1bb46-c115-44aa-934d-7c82e5aeb05e",
+        type: "class",
+        name: "reportlist-container",
+        styleLess: '"width:100%"',
+        children: [],
+        comb: "",
+        namespace: "",
+        selector: null,
+        variants: { small: { styleLess: ["width:100%"] } },
+      },
+    ],
+    assets: {},
+  },
+};
